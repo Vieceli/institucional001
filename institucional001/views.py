307fd32e1d98758965af5338dd5a649e5908fd84
Daryan cunha <daryan@clippermagazine.com.br>
2011-06-17 12:31:24 -0300

    teste

--- institucional001/views.py
+++ institucional001/views.py
@@ -5,7 +5,7 @@ def file_not_found_404(request):
     return render_to_response('404.html', locals(),
                               context_instance=RequestContext(request))
 def server_error_500(request):
-    page_title = u'Erro no Servidor huahuahu'
+    page_title = u'Erro no Servidor'
     return render_to_response('500.html', locals(),
                               context_instance=RequestContext(request))   
     
