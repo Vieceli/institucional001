78d272f32ef921b5f49f651374863c559d7d9db4
Daryan cunha <daryan@clippermagazine.com.br>
2011-06-30 16:25:43 -0300

    mudanca_de_layout

---
+++ institucional001/media/js/menu_links.js
@@ -0,0 +1,56 @@
+
+<!--
+
+function mouseOver1()
+{
+document.getElementById("menu_link_in").src ="/media/img/menu_inicio.png";
+}
+function mouseOut()
+{
+document.getElementById("menu_link_in").src ="/media/img/menu_inicio2.png";
+}
+
+
+function mouseOver2()
+{
+document.getElementById("menu_link_an").src ="/media/img/menu_anuncie.png";
+}
+function mouseOut()
+{
+document.getElementById("menu_link_an").src ="/media/img/menu_anuncie2.png";
+}
+
+
+function mouseOver3()
+{
+document.getElementById("menu_link_ca").src ="/media/img/menu_casos.png";
+}
+function mouseOut()
+{
+document.getElementById("menu_link_ca").src ="/media/img/menu_casos2.png";
+}
+
+
+function mouseOver4()
+{
+document.getElementById("menu_link_re").src ="/media/img/menu_revistas.png";
+}
+function mouseOut()
+{
+document.getElementById("menu_link_re").src ="/media/img/menu_revistas2.png";
+}
+
+
+function mouseOver5()
+{
+document.getElementById("menu_link_co").src ="/media/img/menu_contato.png";
+}
+function mouseOut()
+{
+document.getElementById("menu_link_co").src ="/media/img/menu_contato2.png";
+}
+
+-->
+
+
+
